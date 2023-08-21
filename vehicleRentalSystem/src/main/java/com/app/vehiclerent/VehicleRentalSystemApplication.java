1197bcd9c895547d26728c1001566b5b1e6eaa5a
onkarbhawar111 <onkarbhawar111@gmail.com>
2023-08-21 11:09:31 +0530

    Hi

--- vehicleRentalSystem/src/main/java/com/app/vehiclerent/VehicleRentalSystemApplication.java
+++ vehicleRentalSystem/src/main/java/com/app/vehiclerent/VehicleRentalSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VehicleRentalSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleRentalSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentalSystemApplication.class, args);
+    }
 
 }
